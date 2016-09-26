b62f9816ecd424587cc72f400934600a9732bc3c
Jose Jo Escobar <jo14343@uvg.edu.gt>
2016-09-26 16:53:27 -0600

    Main

--- src/Main.java
+++ src/Main.java
@@ -26,5 +26,51 @@ public class Main {
         }
     }
 
+    /**
+     * Insert a word into the dictionary.
+     * @param string The word to insert.
+     */
+    public void insert(String string) {
+        if (!roots.containsKey(string.charAt(0))) {
+            roots.put(string.charAt(0), new Node());
+        }
+
+        insertWord(string.substring(1),roots.get(string.charAt(0)));
+    }
+
+    //Recursive method that inserts a new word into the trie tree.
+    private void insertWord(String string, Node node) {
+        final Node nextChild;
+        if (node.children.containsKey(string.charAt(0))) {
+            nextChild = node.children.get(string.charAt(0));
+        } else {
+            nextChild = new Node();
+            node.children.put(string.charAt(0), nextChild);
+        }
+
+        if (string.length() == 1) {
+            nextChild.endOfWord = true;
+            return;
+        } else {
+            insertWord(string.substring(1),nextChild);
+        }
+    }
+
+
+    //Recursive method that searches through the Trie Tree to find the value.
+    private boolean searchFor(String string, Node node) {
+        if (string.length()==0) {
+            if (node.endOfWord) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        if (node.children.containsKey(string.charAt(0))) {
+            return searchFor(string.substring(1),node.children.get(string.charAt(0)));
+        } else {
+            return false;
+        }
     }
 }
